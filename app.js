e91c11f82f1a722020aa692e5432f230927bbbfa
Vincent Fuertez <vfuertez@gmail.com>
2022-10-25 05:19:42 -0400

    update

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const baseURL= "https://pokeapi.co/api/v2/pokemon/"
 console.log(baseURL)
 
 function searchPokemon() {
+    
     const name = document.querySelector("#pokemonName").value;
     const url = `${baseURL}${name}`
 
     $.ajax(url)
     .then((data) => {
+        console.log(data)
         document.querySelector(".pokemonDisplay").innerHTML = `
         <div>
     <img src="${data.sprites['front_default']}" alt="${data.name}">
@@ -14,12 +16,11 @@ function searchPokemon() {
 <div class="pokemonInfo">
 <h1>${data.name}</h1>
 <p>${data.types.map( type => type.type.name).join(', ')}</p>
+<p></p>
 </div>
         `
     })
-    // name.preventDefault();
+    data.preventDefault();
 }
-
-//searchPokemon("charmander")
 
 document.querySelector("#search").addEventListener("click", searchPokemon);

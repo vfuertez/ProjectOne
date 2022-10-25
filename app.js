cb7c750ec3065e47cb09ce173bc85a85abb67ed0
Vincent Fuertez <vfuertez@gmail.com>
2022-10-25 03:02:15 -0400

    "daily update"

--- app.js
+++ app.js
@@ -1,28 +1,25 @@
-document.getElementById("search").addEventListener("click", searchPokemon);
-
-
-
 const baseURL= "https://pokeapi.co/api/v2/pokemon/"
 console.log(baseURL)
 
-function searchPokemon(name) {
-    //const name = document.querySelector("#pokemonName")
+function searchPokemon() {
+    const name = document.querySelector("#pokemonName").value;
     const url = `${baseURL}${name}`
 
     $.ajax(url)
     .then((data) => {
-        document.querySelector(".container_1").innerHTML = `
+        document.querySelector(".pokemonDisplay").innerHTML = `
         <div>
-    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png" alt="">
+    <img src="${data.sprites['front_default']}" alt="${data.name}">
 </div>
 <div class="pokemonInfo">
-<h1>${name}</h1>
+<h1>${data.name}</h1>
 <p>${data.types.map( type => type.type.name).join(', ')}</p>
 </div>
         `
-        //console.log(data)
     })
     // name.preventDefault();
 }
 
-searchPokemon("charizard")
+//searchPokemon("charmander")
+
+document.querySelector("#search").addEventListener("click", searchPokemon);
